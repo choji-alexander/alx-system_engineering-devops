e54b782b8fb6c306dc79db0d3601c6191804a31f
choji-alexander <chojialexander@gmail.com>
2023-08-29 05:06:11 -0700

    initial commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Create a file using Puppet
+# Create file in /tmp
 
-file { '/tmp/holberton':
-  ensure  => 'present',
-  content => 'I love Puppet',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744'
-}
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
